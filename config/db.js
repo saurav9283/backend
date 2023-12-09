0cdd7946a759353e610ce9eda2e8758f3150bb1b
Saurav <sauravkumar00108@gmail.com>
2023-12-10 00:00:03 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -3,14 +3,13 @@ const CryptoModel = require("../model/Crypto.schema");
 
 const saveCryptoDataToDB = require("../utils/saveCryptoDataToDB");
 
-const url = "mongodb+srv://saurav:saurav@cluster0.lydlfoa.mongodb.net/app?retryWrites=true&w=majority";
+const url = process.env.MONGO_URL;
 
 const  connectDB= async () => {
     try {
         await mongoose.connect(url);
         console.log("Database Connected!");
-        await CryptoModel.collection.drop() 
-        // Drop the collection before adding new data
+        await CryptoModel.collection.drop() // Drop the collection before adding new data
 
         console.log("Collection Dropped! For adding New Data")
         saveCryptoDataToDB();
